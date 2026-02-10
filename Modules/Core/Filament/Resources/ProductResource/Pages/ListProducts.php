922c2c255a154a59f1ad7c5f2e4d95fb0af16f87
Nabil <nabilstudioapp@gmail.com>
2026-02-10 19:30:57 +0700

    Update Import Product

--- Modules/Core/Filament/Resources/ProductResource/Pages/ListProducts.php
+++ Modules/Core/Filament/Resources/ProductResource/Pages/ListProducts.php
@@ -15,7 +15,14 @@ class ListProducts extends ListRecords
         return [
             Actions\CreateAction::make(),
             Actions\ImportAction::make()
-                ->importer(\App\Filament\Imports\ProductImporter::class),
+                ->importer(\App\Filament\Imports\ProductImporter::class)
+                ->label('Import Products')
+                ->color('gray') 
+                ->modalDescription(fn () => new \Illuminate\Support\HtmlString('Download example CSV: <a href="#" wire:click.prevent="mountAction(\'downloadExample\')">Click here</a>')),
+            Actions\Action::make('downloadExample')
+                ->label('Download Example CSV')
+                ->hidden()
+                ->action(fn () => response()->download(public_path('examples/product-import-example.csv'))),
         ];
     }
 }
